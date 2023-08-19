aeee116356922907d508761288b04ec37ad04f8a
Viacheslav Kostenko <103434668+Bezobrazz@users.noreply.github.com>
2023-08-19 21:28:58 +0300

    Change color in progress bar

--- src/components/InkInfoPanel.jsx
+++ src/components/InkInfoPanel.jsx
@@ -1,37 +1,3 @@
-// import React from "react";
-// import "./InkInfoPanel.css";
-
-// function InkInfoPanel({ totalTimeInSeconds, onReset }) {
-//   const maxHours = 50;
-//   const filledPercentage = (totalTimeInSeconds / (maxHours * 3600)) * 100;
-
-//   const formatTime = (seconds) => {
-//     const hours = Math.floor(seconds / 3600);
-//     const minutes = Math.floor((seconds % 3600) / 60);
-//     const remainingSeconds = seconds % 60;
-
-//     return `${hours} год ${minutes} хв ${remainingSeconds} сек`;
-//   };
-
-//   return (
-//     <div className="ink-info-panel">
-//       <h2>Інформація про мастило</h2>
-//       <div className="ink-progress">
-//         <div
-//           className="ink-progress-bar"
-//           style={{ width: `${filledPercentage}%` }}
-//         ></div>
-//       </div>
-//       <p>Загальний час роботи генератора: {formatTime(totalTimeInSeconds)}</p>
-//       <button className="btn btn-danger" onClick={onReset}>
-//         Скинути відлік
-//       </button>
-//     </div>
-//   );
-// }
-
-// export default InkInfoPanel;
-
 import React from "react";
 import "./InkInfoPanel.css";
 
